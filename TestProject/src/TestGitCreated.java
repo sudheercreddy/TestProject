90ec6796b2b997133ae0872de686e164bc714c79
abc <sudheerqa.selenium@gmail.com>
2020-07-23 23:04:19 -0500

    Added methods to class

--- TestProject/src/TestGitCreated.java
+++ TestProject/src/TestGitCreated.java
@@ -1,7 +1,22 @@
 
 public class TestGitCreated {
 	
-	public void innn() {
+	public void indfdgsnn() {
+		System.out.println("hiihihi");
+	}
+	public void inndfn() {
+		System.out.println("hiihihi");
+	}
+	public void innndffd() {
+		System.out.println("hiihihi");
+	}
+	public void innndfdf() {
+		System.out.println("hiihihi");
+	}
+	public void innnjh() {
+		System.out.println("hiihihi");
+	}
+	public void innnj() {
 		System.out.println("hiihihi");
 	}
 
