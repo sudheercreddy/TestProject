90881e8b265d8e86da40d697e49701bf1018f152
abc <sudheerqa.selenium@gmail.com>
2020-07-23 21:51:30 -0500

    Hellow

--- TestProject/src/TestGitCreated.java
+++ TestProject/src/TestGitCreated.java
@@ -1,7 +1,14 @@
 
 public class TestGitCreated {
+	
+	public void innn() {
+		System.out.println("hiihihi");
+	}
 
 	public static void main(String[] args) {
+		System.out.println("Added to GIT");
+		System.out.println("Added to GIT");
+		System.out.println("Added to GIT");
 		System.out.println("Added to GIT");
 
 	}
